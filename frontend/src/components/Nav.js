de7572933c0efea6fe1a630fd2c0103f4e721de4
Owais Khan <45107209+kowais915@users.noreply.github.com>
2023-02-21 07:18:04 +0500

    added the navbar

---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,26 @@
+
+// component for the navbar of the app
+
+const Nav = () => {
+    return (  
+        <div className="nav">
+
+            <div>
+                <h1>Study Hours</h1>
+            </div>
+            
+            <div>
+                <ul className = "links">
+                    <a href="#"><li>Home</li></a>
+                    <a href="#"><li>Create</li></a>
+                    <a href="#"><li>About</li></a>
+                </ul>
+            </div>
+
+        </div>
+
+
+    );
+}
+ 
+export default Nav;
