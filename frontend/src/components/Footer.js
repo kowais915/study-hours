fca77555c437744acceed533693a93d1e90b0a6a
kowais915 <kowais915@gmail.com>
2023-02-24 00:00:54 +0500

    working on footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = ()=>{
+    return(
+
+        <div class="footer">
+
+            <div class="logo">
+
+            </div>
+
+            <div class="footer nav">
+
+            </div>
+
+        </div>
+    )
+}
