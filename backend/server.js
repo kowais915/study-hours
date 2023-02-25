3413c35b3ad0ada988dbfc77cf2912c528290cb2
Owais Khan <45107209+kowais915@users.noreply.github.com>
2023-02-25 23:14:42 +0500

    generating docs for the api via swagger

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 require('dotenv').config();
 const mongoose = require('mongoose');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 const port = process.env.PORT || 4000;
 
 // the router where users will add new sessions
@@ -11,7 +13,25 @@ const studyRouter = require('./routes/studyHours');
 const app = express();
 
 
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: "The Study Hours API. ",
+            description: "This is the Study Hours api.  This is written in Express.js ", 
+            contact: {
+                name: "Owais Khan"
+            }, 
+            servers: ['http://localhost:4000']
+        }
+    }, 
 
+    apis: ['server.js']
+};
+
+
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 // middlewares
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -36,6 +56,17 @@ mongoose.connect(process.env.MONGODB_URI)
 
 
 // routing begins here
+// Routes
+/**
+ * @swagger
+ * /:
+ *  get: 
+ *    description: Use to request the welcome page.
+ *    responses:
+ *      '200': 
+ *        description: A successful response.
+ */
+
 app.get('/', (req, res)=>{
     res.json({
         msg: "Welcome to the study hour website."
